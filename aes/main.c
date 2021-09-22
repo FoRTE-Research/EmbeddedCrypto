d612ff12adf5a6d0f1cb810200c88547389fdc84
Cindy Ryu <zeezooryu@gmail.com>
2021-09-22 11:15:52 -0400

    Changed the name of the file and included definition for MSP432P401R

--- aes/main.c
+++ aes/main.c
@@ -1,6 +1,9 @@
-
+/* need to choose which AES implementation to run */
 #define gladman_aes
 #define tiny_aes
+
+/* need to uncomment if the board you are using is MSP432P401R */
+// #define msp432p401r
 
 #ifdef gladman_aes
 #include "gladman/aestst.h"
@@ -8,6 +11,10 @@
 
 #ifdef tiny_aes
 #include "tiny_aes/aes.h"
+#endif
+
+#ifdef msp432p401r
+#include "msp.h"
 #endif
 
 int main(void) {
