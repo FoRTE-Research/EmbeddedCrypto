3c72bed320dafcb223abc581d2a3dccf9d2d101b
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2021-10-13 02:19:50 -0400

    Got rid of test_AES ... and trimmed functions

--- aes/tiny_aes/aes.c
+++ aes/tiny_aes/aes.c
@@ -38,7 +38,6 @@ NOTE:   String length must be evenly divisible by 16byte (str_len % 16 == 0)
 #include <string.h> // CBC mode, for memset
 #include <stdio.h>
 #include "aes.h"
-#include "../config.h"
 
 /*****************************************************************************/
 /* Defines:                                                                  */
@@ -74,7 +73,6 @@ NOTE:   String length must be evenly divisible by 16byte (str_len % 16 == 0)
 typedef uint8_t state_t[4][4];
 
 
-
 // The lookup-tables are marked const so they can be placed in read-only storage instead of RAM
 // The numbers below can be computed dynamically trading ROM for RAM - 
 // This can be useful in (embedded) bootloader applications, where ROM is often limited.
@@ -97,7 +95,6 @@ static const uint8_t sbox[256] = {
   0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
   0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 };
 
-#if (defined(CBC) && CBC == 1) || (defined(ECB) && ECB == 1)
 static const uint8_t rsbox[256] = {
   0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
   0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
@@ -115,9 +112,9 @@ static const uint8_t rsbox[256] = {
   0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
   0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
   0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d };
-#endif
 
-// The round constant word array, Rcon[i], contains the values given by 
+
+// The round constant word array, Rcon[i], contains the values given by
 // x to the power (i-1) being powers of x (x is denoted as {02}) in the field GF(2^8)
 static const uint8_t Rcon[11] = {
   0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36 };
@@ -136,12 +133,6 @@ static const uint8_t Rcon[11] = {
 /*****************************************************************************/
 /* Private functions:                                                        */
 /*****************************************************************************/
-/*
-static uint8_t getSBoxValue(uint8_t num)
-{
-  return sbox[num];
-}
-*/
 #define getSBoxValue(num) (sbox[(num)])
 
 // This function produces Nb(Nr+1) round keys. The round keys are used in each round to decrypt the states. 
@@ -149,7 +140,7 @@ static void KeyExpansion(uint8_t* RoundKey, const uint8_t* Key)
 {
   unsigned i, j, k;
   uint8_t tempa[4]; // Used for the column/row operations
-  
+
   // The first round key is the key itself.
   for (i = 0; i < Nk; ++i)
   {
@@ -185,7 +176,7 @@ static void KeyExpansion(uint8_t* RoundKey, const uint8_t* Key)
         tempa[3] = u8tmp;
       }
 
-      // SubWord() is a function that takes a four-byte input word and 
+      // SubWord() is a function that takes a four-byte input word and
       // applies the S-box to each of the four bytes to produce an output word.
 
       // Function Subword()
@@ -326,14 +317,9 @@ static uint8_t Multiply(uint8_t x, uint8_t y)
 
 #endif
 
-#if (defined(CBC) && CBC == 1) || (defined(ECB) && ECB == 1)
-/*
-static uint8_t getSBoxInvert(uint8_t num)
-{
-  return rsbox[num];
-}
-*/
+
 #define getSBoxInvert(num) (rsbox[(num)])
+
 
 // MixColumns function mixes the columns of the state matrix.
 // The method used to multiply may be difficult to understand for the inexperienced.
@@ -398,7 +384,6 @@ static void InvShiftRows(state_t* state)
   (*state)[2][3] = (*state)[3][3];
   (*state)[3][3] = temp;
 }
-#endif // #if (defined(CBC) && CBC == 1) || (defined(ECB) && ECB == 1)
 
 // Cipher is the main function that encrypts the PlainText.
 static void Cipher(state_t* state, const uint8_t* RoundKey)
@@ -426,7 +411,6 @@ static void Cipher(state_t* state, const uint8_t* RoundKey)
   AddRoundKey(Nr, state, RoundKey);
 }
 
-#if (defined(CBC) && CBC == 1) || (defined(ECB) && ECB == 1)
 static void InvCipher(state_t* state, const uint8_t* RoundKey)
 {
   uint8_t round = 0;
@@ -450,36 +434,29 @@ static void InvCipher(state_t* state, const uint8_t* RoundKey)
   }
 
 }
-#endif // #if (defined(CBC) && CBC == 1) || (defined(ECB) && ECB == 1)
 
 /*****************************************************************************/
 /* Public functions:                                                         */
 /*****************************************************************************/
-void AES_encrypt(const struct AES_ctx* ctx, uint8_t* buf)
+
+void AES_encrypt(uint8_t key[], uint8_t in[])
 {
+    struct AES_ctx ctx;
+    AES_init_ctx(&ctx, key);
+
+    const struct AES_ctx* cctx = &ctx;
+
     // The next function call encrypts the PlainText with the Key using AES algorithm.
-    Cipher((state_t*)buf, ctx->RoundKey);
+    Cipher((state_t*)in, cctx->RoundKey);
 }
 
-void AES_decrypt(const struct AES_ctx* ctx, uint8_t* buf)
+void AES_decrypt(uint8_t key[], uint8_t out[])
 {
+    struct AES_ctx ctx;
+    AES_init_ctx(&ctx, key);
+
+    const struct AES_ctx* cctx = &ctx;
+
     // The next function call decrypts the PlainText with the Key using AES algorithm.
-    InvCipher((state_t*)buf, ctx->RoundKey);
-}
-
-int test_AES_encrypt(uint8_t key[], uint8_t in[], uint8_t out[])
-{
-    struct AES_ctx ctx;
-    uint8_t i;
-
-    AES_init_ctx(&ctx, key);
-    AES_encrypt(&ctx, in);
-}
-
-int test_AES_decrypt(uint8_t key[], uint8_t in[], uint8_t out[])
-{
-    struct AES_ctx ctx;
-
-    AES_init_ctx(&ctx, key);
-    AES_decrypt(&ctx, in);
+    InvCipher((state_t*)out, cctx->RoundKey);
 }
