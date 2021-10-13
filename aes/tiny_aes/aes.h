02de055fe06eb47be817a6574a10ccc36e96b5a7
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2021-10-13 02:20:19 -0400

    Minor update on aes.h

--- aes/tiny_aes/aes.h
+++ aes/tiny_aes/aes.h
@@ -3,7 +3,23 @@
 
 #include <stdint.h>
 #include <stddef.h>
-#include "../config.h"
+
+/*****************************************************************************/
+/* Defines:                                                                  */
+/*****************************************************************************/
+// The number of columns comprising a state in AES. This is a constant in AES. Value=4
+#define Nb 4
+
+#if defined(AES_256) && (AES_256 == 1)
+    #define Nk 8
+    #define Nr 14
+#elif defined(AES_192) && (AES_192 == 1)
+    #define Nk 6
+    #define Nr 12
+#else
+    #define Nk 4        // The number of 32 bit words in a key.
+    #define Nr 10       // The number of rounds in AES Cipher.
+#endif
 
 #define AES_BLOCKLEN 16 // Block length in bytes - AES is 128b block only
 
@@ -24,8 +40,7 @@ struct AES_ctx
 };
 
 void AES_init_ctx(struct AES_ctx* ctx, const uint8_t* key);
-
-void AES_encrypt(const struct AES_ctx* ctx, uint8_t* buf);
-void AES_decrypt(const struct AES_ctx* ctx, uint8_t* buf);
+void AES_encrypt(uint8_t key[], uint8_t in[]);
+void AES_decrypt(uint8_t key[], uint8_t out[]);
 
 #endif // _AES_H_
