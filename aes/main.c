fa20e19cf0d403fe9f620212e33f183e283dfd49
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2021-10-13 15:44:23 -0400

    Update main.c

--- aes/main.c
+++ aes/main.c
@@ -1,7 +1,7 @@
 /** need to choose which AES implementation to run **/
-//#define gladman_aes
-#define tiny_aes
-//#define mbedtls_aes
+#define gladman_aes
+//define tiny_aes
+//define mbedtls_aes
 
 /** need to uncomment if the board you are using is MSP432P401R **/
 #define msp432p401r
@@ -44,21 +44,23 @@ uint8_t pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d,
                  0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
 
 // GLADMAN NEEDS A LOT OF FIX
+// defines for init and decrypt as well
 #if defined( gladman_aes )
 #ifdef AES_128
-// defines for init and decrypt as well
-#define test_encrypt(key, pt, ct) aes_gladman_128(key, pt, ct);
-#define test_decrypt(key, pt, ct) aes_d_gladman_128(key, pt, ct);
+#define test_encrypt(key, pt, ct) aes_gladman_128_encrypt(key, pt, ct);
+#define test_decrypt(key, pt, ct) aes_gladman_128_decrypt(key, pt, ct);
 #elif AES_192
-#define test_encrypt(key, pt, ct) aes_gladman_192(key, pt, ct);
-#else AES_256
-#define test_encrypt(key, pt, ct) aes_gladman_256(key, pt, ct);
+#define test_encrypt(key, pt, ct) aes_gladman_192_encrypt(key, pt, ct);
+#define test_decrypt(key, pt, ct) aes_gladman_192_decrypt(key, pt, ct);
+#else // AES_256
+#define test_encrypt(key, pt, ct) aes_gladman_256_encrypt(key, pt, ct);
+#define test_decrypt(key, pt, ct) aes_gladman_256_decrypt(key, pt, ct);
 #endif
 #endif
 
 #if defined( tiny_aes )
-#define test_encrypt(key, pt) AES_encrypt(key, pt);
-#define test_decrypt(key, ct) AES_decrypt(key, ct);
+#define test_encrypt(key, pt, ct) AES_encrypt(key, pt, ct);
+#define test_decrypt(key, ct, pt) AES_decrypt(key, ct, pt);
 #endif
 
 #if defined( mbedtls_aes )
@@ -68,19 +70,21 @@ uint8_t pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d,
 int main(void)
 {
     // STILL TO DO FOR ZEEZOO
-    // init_aes()
+    // init_aes()=> Moving the init to main.c causes main.c to become too big. It will add about 300 lines of code.
+    // In aes.c for tiny-AES, there are multiple functions that need to be moved in order for the struct to be created here.
+    // Is it still going to be better to have the init here?
+
     // encrypt() or decrypt() possibly many times
 
 
     // Choose the function to be called
-    // test_encrypt(key, pt);
-    test_decrypt(key, ct);
-
+    test_encrypt(key, pt, ct);
+    // test_decrypt(key, ct, pt);
 
     // Check the result to see whether AES algorithm is correctly working or not
     if (0 == memcmp((char*) ct, (char*) pt, 16))
     {
-        return 0; // Successful
+        return 0; // Success
     }
     else
     {
