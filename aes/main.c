010f12310215d0c02801c0c8eb07bd657998280e
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2021-10-19 22:51:08 -0400

    Update initialize - working for tiny AES

--- aes/main.c
+++ aes/main.c
@@ -1,6 +1,6 @@
 /** need to choose which AES implementation to run **/
 #define gladman_aes
-//define tiny_aes
+//#define tiny_aes
 //define mbedtls_aes
 
 /** need to uncomment if the board you are using is MSP432P401R **/
@@ -42,10 +42,20 @@ uint8_t ct[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06, 0x4b,
                  0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
 uint8_t pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d,
                  0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
+struct AES_ctx ctx;
+
+#ifdef AES_128
+#define keysize = 128;
+#elif AES_192
+#define keysize = 192;
+#elif AES_256
+#define keysize = 256;
+#endif
 
 // GLADMAN NEEDS A LOT OF FIX
 // defines for init and decrypt as well
 #if defined( gladman_aes )
+#define init_aes(key, pt, ct, keysize) gladman_init(key, pt, ct, keysize);
 #ifdef AES_128
 #define test_encrypt(key, pt, ct) aes_gladman_128_encrypt(key, pt, ct);
 #define test_decrypt(key, pt, ct) aes_gladman_128_decrypt(key, pt, ct);
@@ -58,10 +68,14 @@ uint8_t pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d,
 #endif
 #endif
 
+
 #if defined( tiny_aes )
-#define test_encrypt(key, pt, ct) AES_encrypt(key, pt, ct);
-#define test_decrypt(key, ct, pt) AES_decrypt(key, ct, pt);
+//#define tiny_aes_init(ctx, key) AES_init_ctx(ctx, key);
+#define init_aes(ctx, key) AES_init_ctx(ctx, key);
+#define test_encrypt(ctx, key, pt, ct) AES_encrypt(ctx, key, pt, ct);
+#define test_decrypt(ctx, key, ct, pt) AES_decrypt(ctx, key, ct, pt);
 #endif
+
 
 #if defined( mbedtls_aes )
 //#define test_encrypt(a, b, c) mbedtls_internal_aes_encrypt( mbedtls_aes_context *ctx, const unsigned char input[16], unsigned char output[16] )
@@ -69,17 +83,15 @@ uint8_t pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d,
 
 int main(void)
 {
-    // STILL TO DO FOR ZEEZOO
-    // init_aes()=> Moving the init to main.c causes main.c to become too big. It will add about 300 lines of code.
-    // In aes.c for tiny-AES, there are multiple functions that need to be moved in order for the struct to be created here.
-    // Is it still going to be better to have the init here?
+    // initialize AES
+    init_aes(&ctx, key); // initialize for tiny AES
+    init_aes(key, pt, ct, keysize); // initialize for gladman AES
 
     // encrypt() or decrypt() possibly many times
 
-
     // Choose the function to be called
-    test_encrypt(key, pt, ct);
-    // test_decrypt(key, ct, pt);
+    // test_encrypt(&ctx, key, pt, ct);
+    // test_decrypt(&ctx, key, ct, pt);
 
     // Check the result to see whether AES algorithm is correctly working or not
     if (0 == memcmp((char*) ct, (char*) pt, 16))
