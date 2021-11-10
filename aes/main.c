3a93915c68ea1e7c4101fde7c71fe28dc795fdfc
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2021-11-10 11:14:10 -0500

    Update main.c

--- aes/main.c
+++ aes/main.c
@@ -1,16 +1,4 @@
-/** need to choose which AES implementation to run **/
-#define gladman_aes
-//#define tiny_aes
-//#define mbedtls_aes
-
-/** need to uncomment if the board you are using is MSP432P401R **/
-#define msp432p401r
-//#define riscv
-
-/** need to define key size **/
-#define AES_128 1
-//#define AES_192 1
-//#define AES_256 1
+#include "test.h" // Change test.h to select different algorithms
 
 /// DO NOT EDIT BELOW  //////////////////////////////////////////
 #include <stdint.h>
