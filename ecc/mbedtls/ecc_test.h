5c2d4813f5490f48502e49f91828d85e750b1fad
William <59912120+williamliao777@users.noreply.github.com>
2021-11-16 15:51:33 -0500

    mbedtls ecdh
    work on macOS well but RISC-V

---
+++ ecc/mbedtls/ecc_test.h
@@ -0,0 +1,7 @@
+
+#ifndef MBEDTLS_TEST_ECDH_ECC_TEST_H
+#define MBEDTLS_TEST_ECDH_ECC_TEST_H
+
+int mbedtls_ecc_test();
+
+#endif //MBEDTLS_TEST_ECDH_ECC_TEST_H
---
+++ ecc/mbedtls/main.c
@@ -0,0 +1,7 @@
+#include "ecc_test.h"
+
+int main( int argc, char *argv[] )
+{
+    mbedtls_ecc_test();
+    return 1;
+}
