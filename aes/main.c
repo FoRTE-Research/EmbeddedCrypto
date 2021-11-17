854ce499e5c517ba3d7e4a9d46fc1bcde312f3f5
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2021-11-17 09:57:35 -0500

    Update check result in AES main.c

--- aes/main.c
+++ aes/main.c
@@ -32,6 +32,7 @@ uint8_t check_encrypt[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06
                  0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
 uint8_t pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d,
                  0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
+
 /** contexts **/
 #ifdef tiny_aes
 struct AES_ctx ctx;
@@ -109,13 +110,11 @@ void test_decrypt() {
     #endif
 }
 
- int check_result() {
-     if (0 == memcmp((char*) pt, (char*) check_encrypt, 16))
-     {
+int check_result() {
+     if (0 == memcmp((char*) pt, (char*) check_encrypt, 16)) {
          return 0; // Success
      }
-     else
-     {
+     else {
          return 1; // Failure
      }
  }
