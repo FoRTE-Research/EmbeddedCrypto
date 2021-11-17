b83ba90355b682adfaac063bd9f566ec3a074707
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2021-11-17 09:56:45 -0500

    Update check result in AES main.c

--- aes/main.c
+++ aes/main.c
@@ -28,6 +28,8 @@ uint8_t key[] = { 0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73,
                   0xdf, 0xf4 };
 uint8_t ct[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06, 0x4b,
                  0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
+uint8_t check_encrypt[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06, 0x4b,
+                 0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
 uint8_t pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d,
                  0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
 /** contexts **/
@@ -107,16 +109,16 @@ void test_decrypt() {
     #endif
 }
 
-// int check_result() {
-//     if (0 == memcmp((char*) pt, (char*) decrypted, 16))
-//     {
-//         return 0; // Success
-//     }
-//     else
-//     {
-//         return 1; // Failure
-//     }
-// }
+ int check_result() {
+     if (0 == memcmp((char*) pt, (char*) check_encrypt, 16))
+     {
+         return 0; // Success
+     }
+     else
+     {
+         return 1; // Failure
+     }
+ }
 
 int main(void)
 {
@@ -129,5 +131,5 @@ int main(void)
     // test_decrypt();
 
     /** Check the result to see whether AES algorithm is correctly working or not **/
-//     check_result();
+    check_result();
 }
