ce65bcd0d519f5ed0d5c89ab097c48ad99c8bd95
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2022-01-12 12:53:36 -0500

    Fixing SysTick timer on AES

--- aes/main.c
+++ aes/main.c
@@ -28,8 +28,8 @@ uint8_t key[] = { 0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73,
                   0xdf, 0xf4 };
 uint8_t ct[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06, 0x4b,
                  0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
-uint8_t check_encrypt[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06, 0x4b,
-                 0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
+uint8_t check_encrypt[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c,
+                            0x06, 0x4b, 0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
 uint8_t pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d,
                  0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
 /** contexts **/
@@ -50,7 +50,8 @@ long keysize = 256;
 #endif
 
 /** Call initialization functions for different AES implementations **/
-void init_aes() {
+void init_aes()
+{
 #ifdef gladman_aes
     gladman_init(key, pt, ct, keysize);
 #endif
@@ -63,68 +64,79 @@ void init_aes() {
 #endif
 }
 
-void test_encrypt() {
+void test_encrypt()
+{
     /** Gladman AES **/
-    #ifdef gladman_aes
-    #ifdef AES_128
+#ifdef gladman_aes
+#ifdef AES_128
     aes_gladman_128_encrypt(key, pt, ct);
     #elif AES_192
     aes_gladman_192_encrypt(key, pt, ct);
-    #else // AES_256
+#else // AES_256
     aes_gladman_256_encrypt(key, pt, ct);
     #endif
-    #endif
+#endif
 
     /** tiny AES **/
-    #ifdef tiny_aes
+#ifdef tiny_aes
     AES_encrypt(&ctx, key, pt, ct);
     #endif
 
     /** MbedTLS AES **/
-    #ifdef mbedtls_aes
+#ifdef mbedtls_aes
     mbedtls_internal_aes_encrypt(&ctx, pt, ct);
     #endif
 }
 
-void test_decrypt() {
+void test_decrypt()
+{
     /** Gladman AES **/
-    #ifdef gladman_aes
-    #ifdef AES_128
+#ifdef gladman_aes
+#ifdef AES_128
     aes_gladman_128_decrypt(key, ct, pt);
     #elif AES_192
     aes_gladman_192_decrypt(key, ct, pt);
-    #else // AES_256
+#else // AES_256
     aes_gladman_256_decrypt(key, ct, pt);
     #endif
-    #endif
+#endif
 
     /** tiny AES **/
-    #ifdef tiny_aes
+#ifdef tiny_aes
     AES_decrypt(&ctx, key, ct, pt);
     #endif
 
     /** MbedTLS AES **/
-    #ifdef mbedtls_aes
+#ifdef mbedtls_aes
     mbedtls_internal_aes_decrypt(&ctx, pt, ct);
     #endif
 }
 
-void SysTick_Init(void){
- SysTick->LOAD = 0x00FFFFFF;
- SysTick->CTRL = 0x00000005;
+int check_result()
+{
+    return memcmp((char*) pt, (char*) check_encrypt, 16);
 }
 
-int check_result() {
-     return memcmp((char*) pt, (char*) check_encrypt, 16);
+volatile uint32_t msTicks = 0; /* Variable to store millisecond ticks */
+
+void SysTick_Handler(void)
+{ /* SysTick interrupt Handler. */
+    msTicks++;
 }
 
 int main(void)
 {
-    int32_t start, end, duration;
+    uint32_t returnCode;
+    uint32_t Totaltime;
 
-//    Clock_Init48MHz(); // it says this makes bus clock 48 MHz but do not quite understand
-    SysTick_Init();
-    start = SysTick->VAL;
+    returnCode = SysTick_Config(SystemCoreClock / 1000); /* Configure SysTick to generate an interrupt every millisecond */
+
+    if (returnCode != 0)
+    { /* Check return code for errors */
+        // Error Handling
+    }
+
+    uint32_t TimeBeforeExecution = msTicks;
 
     /** initialize AES **/
     init_aes();
@@ -134,11 +146,10 @@ int main(void)
     test_encrypt();
     // test_decrypt();
 
-    end = SysTick->VAL;
-    duration = 0x00FFFFFF&(start-end); // Is this duration in milliseconds -- finding this out
-
     /** Check the result to see whether AES algorithm is correctly working or not **/
     check_result();
 
-    return 0;
+    Totaltime = msTicks - TimeBeforeExecution;
+
+    while(1);
 }
