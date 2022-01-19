b86857f0e703d6238c7d727c792c1da568e3ba6c
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2022-01-19 14:57:23 -0600

    Update test.h to define slow tiny AES

--- aes/test.h
+++ aes/test.h
@@ -1,16 +1,10 @@
-/*
- * test.h
- *
- *  Created on: Nov 10, 2021
- *      Author: zeezooryu
- */
-
 #ifndef AES_TEST_H_
 #define AES_TEST_H_
 
 /** need to choose which AES implementation to run **/
 //#define gladman_aes
-#define tiny_aes
+#define slow_tiny_aes
+//#define tiny_aes
 //#define mbedtls_aes
 
 /** need to uncomment if the board you are using is MSP432P401R **/
@@ -18,8 +12,8 @@
 //#define riscv
 
 /** need to define key size **/
-//#define AES_128 1
-#define AES_192 1
+#define AES_128 1
+//#define AES_192 1
 //#define AES_256 1
 
 /** can define block chaining **/
