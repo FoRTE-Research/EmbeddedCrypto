2ef1a520948f823414357cbcb0c5eadf398c3be4
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2022-05-18 07:41:38 -0400

    Remove MBEDTLS_PRIVATE on variables to debug

--- aes/mbedtls/aes.h
+++ aes/mbedtls/aes.h
@@ -75,16 +75,19 @@ extern "C" {
  */
 typedef struct mbedtls_aes_context
 {
-    int MBEDTLS_PRIVATE(nr);                     /*!< The number of rounds. */
-    uint32_t *MBEDTLS_PRIVATE(rk);               /*!< AES round keys. */
-    uint32_t MBEDTLS_PRIVATE(buf)[68];           /*!< Unaligned data buffer. This buffer can
-                                     hold 32 extra Bytes, which can be used for
-                                     one of the following purposes:
-                                     <ul><li>Alignment if VIA padlock is
-                                             used.</li>
-                                     <li>Simplifying key expansion in the 256-bit
-                                         case by generating an extra round key.
-                                         </li></ul> */
+    // int MBEDTLS_PRIVATE(nr);                     /*!< The number of rounds. */
+    // uint32_t *MBEDTLS_PRIVATE(rk);               /*!< AES round keys. */
+    // uint32_t MBEDTLS_PRIVATE(buf)[68];           /*!< Unaligned data buffer. This buffer can
+    //                                  hold 32 extra Bytes, which can be used for
+    //                                  one of the following purposes:
+    //                                  <ul><li>Alignment if VIA padlock is
+    //                                          used.</li>
+    //                                  <li>Simplifying key expansion in the 256-bit
+    //                                      case by generating an extra round key.
+    //                                      </li></ul> */
+    int nr;
+    uint32_t *rk;
+    uint32_t (buf)[68];
 }
 mbedtls_aes_context;
 
