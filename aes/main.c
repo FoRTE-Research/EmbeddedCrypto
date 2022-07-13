1d168a088e95d2320d29d18c29d90cc41db865be
zeezooryu <42547568+zeezooryu@users.noreply.github.com>
2022-07-13 06:29:07 -0400

    Update main.c

--- aes/main.c
+++ aes/main.c
@@ -41,9 +41,9 @@ uint8_t key[] = { 0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73,
                   0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x1f, 0x35, 0x2c, 0x07,
                   0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14,
                   0xdf, 0xf4 };
-uint8_t check_encrypt[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c,
+uint8_t expected_ct[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c,
                             0x06, 0x4b, 0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
-uint8_t check_decrypt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96,
+uint8_t expected_pt[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96,
                             0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
 #ifdef AES_CBC
 uint8_t pt[MSG_LNGTH];
@@ -148,18 +148,22 @@ void test_decrypt()
 #endif
 }
 
-int check_result(char *p)
-{
-    char *process = p;
+/******************************
+ *
+ * Function to verify encryption
+ *
+ ******************************/
+int check_encrypt() {
+    return memcmp((char*) ct, (char*) expected_ct, MSG_LNGTH);
+}
 
-    if (strcmp(process, "e"))
-    { // If the process is encryption
-        return memcmp((char*) pt, (char*) check_encrypt, MSG_LNGTH);
-    }
-    else
-    {
-        return memcmp((char*) check_decrypt, (char*) ct, MSG_LNGTH); // If the process is decryption
-    }
+/******************************
+ *
+ * Function to verify decryption
+ *
+ ******************************/
+int check_decrypt() {
+    return memcmp((char*) expected_pt, (char*) pt, MSG_LNGTH);
 }
 
 #ifdef AES_CBC
@@ -221,14 +225,14 @@ void main(void)
 
     /** Choose the function to be called **/
     /** Encrypt or decrypt possibly many times **/
-    //test_encrypt();
-    test_decrypt();
+    test_encrypt();
+    //test_decrypt();
     //aes_encrypt_cbc(sizeof(pt));
     //aes_decrypt_cbc(sizeof(ct));
 
     /** Check the result to see whether AES algorithm is correctly working or not **/
-    //check_result("e"); // Check the validity of an encryption method
-    check_result("d"); // Check the validity of a decryption method
+    check_encrypt(); // Check the validity of an encryption method
+    //check_decrypt(); // Check the validity of a decryption method
 
     volatile unsigned int elapsed = getElapsedTime();
 
